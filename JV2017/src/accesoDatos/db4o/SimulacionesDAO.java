2db00e15a0e7f1649502a2b89e0544400e98bb45
dam17-08 <dam17-08@c16x14.iescierva.net>
2018-05-28 12:51:53 +0200

    PruebaGitPersonal

---
+++ JV2017/src/accesoDatos/db4o/SimulacionesDAO.java
@@ -0,0 +1,57 @@
+package accesoDatos.db4o;
+import accesoDatos.*;
+import modelo.*;
+import util.*;
+
+public class SimulacionesDAO implements OperacionesDAO {
+
+	@Override
+	public Object obtener(String id) throws DatosException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object obtener(Object obj) throws DatosException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void alta(Object obj) throws DatosException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object baja(String id) throws DatosException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void actualizar(Object obj) throws DatosException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String listarDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrarTodo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cerrar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
